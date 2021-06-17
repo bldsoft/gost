d022a18e122f12ec59143be5163078540f57e642
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2021-06-17 17:34:00 +0300

    [STR-1325] up chi version

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/ghodss/yaml v1.0.0
-	github.com/go-chi/chi v1.5.2
+	github.com/go-chi/chi/v5 v5.0.3
 	github.com/mattn/go-colorable v0.0.9
 	github.com/rs/zerolog v1.20.0
 	github.com/spf13/viper v1.7.1
--- server/server.go
+++ server/server.go
@@ -9,8 +9,8 @@ import (
 	"time"
 
 	"github.com/bldsoft/gost/log"
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type IMicroservice interface {
