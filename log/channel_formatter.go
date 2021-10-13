c97a637dfba74f5e754d802ee15c525ce02a5418
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2021-10-13 18:12:41 +0300

    [STR-1365] moved RequestInfo to a separate file

--- log/channel_formatter.go
+++ log/channel_formatter.go
@@ -7,54 +7,6 @@ import (
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-type RequestMethodType byte
-type ResponseCodeType uint16
-
-const (
-	GET RequestMethodType = iota
-	POST
-	PUT
-	DELETE
-	OPTIONS
-
-	ERROR
-)
-
-type RequestInfo struct {
-	RequestMethod RequestMethodType
-	Path          string
-	ClientIp      string
-	UserAgent     string
-	ResponseCode  ResponseCodeType
-	Size          uint32
-	RequestTime   int64
-	HandleTime    uint32
-	Instance      string
-	RequestId     string
-	Error         string
-}
-
-func NewRequestInfo() *RequestInfo {
-	return &RequestInfo{RequestTime: time.Now().Unix()}
-}
-
-func GetRequestMethodType(type_str string) RequestMethodType {
-	switch type_str {
-	case "GET":
-		return GET
-	case "POST":
-		return POST
-	case "PUT":
-		return PUT
-	case "DELETE":
-		return DELETE
-	case "OPTIONS":
-		return OPTIONS
-	default:
-		return ERROR
-	}
-}
-
 type ChannelFormatter struct {
 	requestC     chan<- *RequestInfo
 	instanseName string
