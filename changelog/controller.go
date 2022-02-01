104f149ae4385bdbaebac303215a043d517fd96d
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2022-02-01 15:02:45 +0300

    [STR-1455] added changelog.Controller

---
+++ changelog/controller.go
@@ -0,0 +1,50 @@
+package changelog
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/bldsoft/gost/controller"
+	"github.com/go-chi/chi/v5"
+)
+
+type Controller struct {
+	controller.BaseController
+	changeLogService IChangeLogService
+}
+
+func NewController(changeLogService IChangeLogService) *Controller {
+	return &Controller{changeLogService: changeLogService}
+}
+
+func (c *Controller) getFilter(r *http.Request) *Filter {
+	query := r.URL.Query()
+	var filter Filter
+	if time, err := strconv.ParseInt(query.Get("startTime"), 10, 64); err == nil && time > 0 {
+		filter.StartTime = time
+	}
+	if time, err := strconv.ParseInt(query.Get("endTime"), 10, 64); err == nil && time > filter.StartTime {
+		filter.EndTime = time
+	}
+	if entityID := query.Get("entityID"); entityID != "" {
+		filter.EntityID = entityID
+	}
+	if collections := query.Get("entities"); collections != "" {
+		filter.Collections = strings.Split(collections, ",")
+	}
+	return &filter
+}
+
+func (c *Controller) GetHandler(w http.ResponseWriter, r *http.Request) {
+	records, err := c.changeLogService.GetRecords(r.Context(), c.getFilter(r))
+	if err != nil {
+		c.ResponseError(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	c.ResponseJson(w, r, records)
+}
+
+func (c *Controller) Mount(r chi.Router) {
+	r.Get("/", c.GetHandler)
+}
