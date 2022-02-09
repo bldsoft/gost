932719a332c62d11a3c95b9b026b54118c3972d6
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2022-02-09 13:01:25 +0300

    [STR-1455] added auth interfaces

---
+++ auth/interface.go
@@ -0,0 +1,39 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/bldsoft/gost/repository"
+)
+
+type Authenticatable interface {
+	Name() string
+	Password() string
+	SetPassword(string)
+}
+
+type AuthenticatablePtr[T any] interface {
+	*T
+	Authenticatable
+}
+
+type IRole interface {
+	comparable
+}
+
+type Authorizable[T IRole] interface {
+	Role() T
+}
+
+type IUserRepository[U any] interface {
+	Insert(ctx context.Context, user U) error
+	FindByName(ctx context.Context, name string) (U, error)
+	Update(ctx context.Context, user U) error
+	Delete(ctx context.Context, user U, options ...*repository.QueryOptions) error
+}
+
+// IAuthService ...
+type IAuthService[T any, U AuthenticatablePtr[T]] interface {
+	SignUp(ctx context.Context, user U) error
+	Login(ctx context.Context, username, password string) (U, error)
+}
