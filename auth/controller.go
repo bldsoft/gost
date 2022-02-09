94bbcc37d83cae7cb4d67f2e0d7db960648bb4cb
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2022-02-09 13:06:09 +0300

    [STR-1455] added auth controller

---
+++ auth/controller.go
@@ -0,0 +1,124 @@
+package auth
+
+import (
+	"net/http"
+
+	"github.com/bldsoft/gost/controller"
+	"github.com/bldsoft/gost/log"
+	"github.com/go-chi/chi/v5"
+	"github.com/gorilla/sessions"
+)
+
+const SessionUserKey = "user"
+
+// AuthController ...
+type AuthController[T any, U AuthenticatablePtr[T]] struct {
+	controller.BaseController
+	sessionStore sessions.Store
+	authService  IAuthService[T, U]
+	cookieName   string
+}
+
+func NewAuthController[U AuthenticatablePtr[T], T any](sessionStore sessions.Store, userRep IUserRepository[U], cookieName string) *AuthController[T, U] {
+	return &AuthController[T, U]{sessionStore: sessionStore, authService: NewAuthService[T, U](userRep), cookieName: cookieName}
+}
+
+func (c *AuthController[T, U]) session(w http.ResponseWriter, r *http.Request) (*sessions.Session, bool) {
+	session, err := c.sessionStore.Get(r, c.cookieName)
+	if err != nil {
+		log.FromContext(r.Context()).ErrorWithFields(log.Fields{"err": err}, "Failed to get session")
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return nil, false
+	}
+	return session, true
+}
+
+func (c *AuthController[T, U]) saveSession(w http.ResponseWriter, r *http.Request, s *sessions.Session) bool {
+	err := c.sessionStore.Save(r, w, s)
+	if err != nil {
+		log.FromContext(r.Context()).ErrorWithFields(log.Fields{"err": err}, "Failed to save session")
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return false
+	}
+	return true
+}
+
+// AuthenticateMiddleware authenticates user and put it into into request context.
+func (c *AuthController[T, U]) AuthenticateMiddleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			session, ok := c.session(w, r)
+			if !ok {
+				return
+			}
+
+			user, ok := session.Values[SessionUserKey].(T)
+			if !ok {
+				http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+				return
+			}
+			next.ServeHTTP(w, WithUserContext(r, &user))
+		})
+
+	}
+}
+
+// SignUp ...
+func (c *AuthController[T, U]) SignUp(w http.ResponseWriter, r *http.Request) {
+	var user T
+	if !c.GetObjectFromBody(w, r, &user) {
+		return
+	}
+
+	if err := c.authService.SignUp(r.Context(), &user); err != nil {
+		c.ResponseError(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	c.ResponseOK(w)
+}
+
+func (c *AuthController[T, U]) Login(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	username, password, ok := r.BasicAuth()
+	if !ok {
+		c.ResponseError(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+	user, err := c.authService.Login(ctx, username, password)
+	if err != nil {
+		log.FromContext(ctx).Infof(err.Error())
+		c.ResponseError(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return
+	}
+
+	session, ok := c.session(w, r)
+	if !ok {
+		return
+	}
+	session.Values[SessionUserKey] = *user
+	if c.saveSession(w, r, session) {
+		c.ResponseOK(w)
+	}
+}
+
+// Logout ...
+func (c *AuthController[T, U]) Logout(w http.ResponseWriter, r *http.Request) {
+	session, ok := c.session(w, r)
+	if !ok {
+		return
+	}
+
+	session.Values[SessionUserKey] = nil
+	session.Options.MaxAge = -1
+
+	if c.saveSession(w, r, session) {
+		c.ResponseOK(w)
+	}
+}
+
+func (c *AuthController[T, U]) Mount(r chi.Router) {
+	r.Post("/signup", c.SignUp)
+	r.Post("/login", c.Login)
+	r.Post("/logout", c.Logout)
+}
--- go.mod
+++ go.mod
@@ -3,17 +3,53 @@ module github.com/bldsoft/gost
 go 1.18
 
 require (
+	github.com/evanphx/json-patch v4.9.0+incompatible
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-chi/chi/v5 v5.0.3
 	github.com/golang-migrate/migrate/v4 v4.15.1
+	github.com/golang/mock v1.6.0
+	github.com/gorilla/sessions v1.2.1
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/mattn/go-colorable v0.1.6
 	github.com/pkg/errors v0.9.1
-	github.com/rs/zerolog v1.20.0
+	github.com/rs/zerolog v1.21.0
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.7.0
 	go.mongodb.org/mongo-driver v1.8.2
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+	golang.org/x/exp v0.0.0-20220209042442-160e291fcf24
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
 
-require github.com/evanphx/json-patch/v5 v5.6.0 // indirect
+require (
+	github.com/cenkalti/backoff/v4 v4.0.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gorilla/securecookie v1.1.1 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mitchellh/mapstructure v1.1.2 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cast v1.3.0 // indirect
+	github.com/spf13/jwalterweatherman v1.0.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.51.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
+)
