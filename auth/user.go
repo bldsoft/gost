1dd866da050cc8a6c59439a4595d0da4744f5210
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2022-02-09 13:02:17 +0300

    [STR-1455] added base user struct

---
+++ auth/user.go
@@ -0,0 +1,33 @@
+package auth
+
+type Creds struct {
+	Username     string `json:"name,omitempty" bson:"name,omitempty"`
+	UserPassword string `json:"password,omitempty" bson:"password,omitempty"`
+}
+
+func (c *Creds) Name() string {
+	return c.Username
+}
+
+func (c *Creds) SetPassword(password string) {
+	c.UserPassword = password
+}
+func (c *Creds) Password() string {
+	return c.UserPassword
+}
+
+type EntityRole[T IRole] struct {
+	UserRole T `json:"role,omitempty" bson:"role,omitempty"`
+}
+
+func (e *EntityRole[T]) Role() T {
+	return e.UserRole
+}
+
+type User[R IRole] struct {
+	Creds         `bson:",inline" json:",inline"`
+	EntityRole[R] `bson:",inline" json:",inline"`
+}
+
+var _ Authenticatable = (*User[int])(nil)
+var _ Authorizable[int] = (*User[int])(nil)
