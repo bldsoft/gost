c150a4f251f0313a58ace9fc733788499edb617f
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2022-02-09 13:03:00 +0300

    [STR-1455] added auth service

---
+++ auth/service.go
@@ -0,0 +1,56 @@
+package auth
+
+import (
+	"context"
+	"fmt"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var ErrWrongPassword = fmt.Errorf("wrong password")
+
+// type PasswordHasher interface {
+// 	HashAndSalt(password []byte) ([]byte, error)
+// 	VerifyPassword(passwordHash, password []byte) error
+// }
+
+// AuthService ...
+type AuthService[T any, U AuthenticatablePtr[T]] struct {
+	userRep IUserRepository[U]
+	// passwordHasher PasswordHasher
+}
+
+// NewAuthService ...
+func NewAuthService[T any, U AuthenticatablePtr[T]](rep IUserRepository[U]) *AuthService[T, U] {
+	return &AuthService[T, U]{userRep: rep}
+}
+
+func (s *AuthService[T, U]) SignUp(ctx context.Context, user U) error {
+	hashedPass, err := s.hashAndSalt(user.Password())
+	if err != nil {
+		return fmt.Errorf("failed to hash password: %v", err)
+	}
+	user.SetPassword(hashedPass)
+	return s.userRep.Insert(ctx, user)
+}
+
+func (s *AuthService[T, U]) Login(ctx context.Context, username, password string) (U, error) {
+	user, err := s.userRep.FindByName(ctx, username)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := s.verifyPassword(user.Password(), password); err != nil {
+		return nil, ErrWrongPassword
+	}
+	return user, nil
+}
+
+func (s *AuthService[T, U]) hashAndSalt(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	return string(hash), err
+}
+
+func (s *AuthService[T, U]) verifyPassword(passwordHash, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password))
+}
