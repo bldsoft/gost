7b08b22375c3887fb3bfafcc564896a0c70e1374
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2022-06-14 18:34:15 +0300

    [STR-1574] added config description; refactored setting default config

--- config/config.go
+++ config/config.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"os"
 	"reflect"
 	"runtime"
 	"strings"
@@ -17,10 +18,31 @@ type IConfig interface {
 	Validate() error
 }
 
-func ReadConfig(config IConfig, prefix string) {
-	config.SetDefaults()
-	ReadFromEnv(config, prefix)
-	if err := config.Validate(); err != nil {
+// ReadConfig reads config from the envrionment and validates it.
+// config can include nested IConfig structs.
+// ReadConfig calls SetDefaults of all IConfig structs, so you don't have to call SetDefaults of nested configs in parent SetDefualts.
+// Do it only if you want to rewrite a child config defaults in the parent config.
+// For the same reason you don't need call Validate of the nested IConfigs in the parent Validate method.
+//
+// See ReadFromEnv for detail of binding config fields with names of environment variables.
+//
+// Set CONFIG_DESCRIPTION_PATH env var to generate Markdown config description.
+// See WriteConfigDescription for detail of the description generating.
+func ReadConfig(config IConfig, envPrefix string) {
+	if err := SetDefaults(config); err != nil {
+		log.Fatalf("Failed to set config defaults: %v", err)
+	}
+
+	path := os.Getenv("CONFIG_DESCRIPTION_PATH")
+	if len(path) > 0 {
+		WriteMarkdownDescription(path, config, envPrefix)
+	}
+
+	if err := ReadFromEnv(config, envPrefix); err != nil {
+		log.Fatalf("Failed read config from environment: %v", err)
+	}
+
+	if err := Validate(config); err != nil {
 		log.Fatalf("Invalid config: %v", err)
 	}
 }
@@ -34,6 +56,31 @@ func FormatEnv(config IConfig) string {
 	)
 }
 
+func foreachConfig(config interface{}, f func(cfg IConfig) error) error {
+	return iterateFields(config, "", nil, nil, func(cfg interface{}) error {
+		if c, ok := cfg.(IConfig); ok {
+			return f(c)
+		}
+		return nil
+	})
+}
+
+// SetDefaults sets default values of config and all nested IConfig structs
+// SetDefaults method of nested IConfig is called before its parent. So you can rewrite default values of the child config in the parent if nessesary
+func SetDefaults(config interface{}) error {
+	return foreachConfig(config, func(cfg IConfig) error {
+		cfg.SetDefaults()
+		return nil
+	})
+}
+
+// Validate validates config and all nested IConfig structs
+func Validate(config interface{}) error {
+	return foreachConfig(config, func(cfg IConfig) error {
+		return cfg.Validate()
+	})
+}
+
 func addPrefix(name string, prefix string) string {
 	if name != "" && prefix != "" {
 		return fmt.Sprintf("%s_%s", prefix, name)
@@ -41,11 +88,38 @@ func addPrefix(name string, prefix string) string {
 	return prefix + name
 }
 
-// ReadFromEnv ...
-func ReadFromEnv(config interface{}, prefix string) error {
-	v := viper.NewWithOptions(viper.KeyDelimiter("."))
+// ReadFromEnv reads config from environment variable
+// The name of environment variable is set via "mapstructure" tag. If tag isn't set, the env name is the struct field name.
+// Prefix for the env variable name consists of envPrefix and "mapstructure" values of all parent structs joined together with "_" (see example)
+func ReadFromEnv(config interface{}, envPrefix string) error {
+	v := viper.NewWithOptions(
+		viper.KeyDelimiter("."),
+	)
 	v.AutomaticEnv()
-	tagToEnvKey := make([]string, 0)
+
+	var tagToEnvKeyStack [][]string
+	return iterateFields(config, envPrefix, func(cfg interface{}) error {
+		tagToEnvKeyStack = append(tagToEnvKeyStack, nil) // push
+		return nil
+	}, func(envVarName, envNamePrefix string, _ reflect.StructField, _ reflect.Value) error {
+		tagToEnvKeyStack[len(tagToEnvKeyStack)-1] = append(tagToEnvKeyStack[len(tagToEnvKeyStack)-1], envVarName, addPrefix(envVarName, envNamePrefix))
+		return v.BindEnv(envVarName)
+	}, func(cfg interface{}) error {
+		top := tagToEnvKeyStack[len(tagToEnvKeyStack)-1]
+		v.SetEnvKeyReplacer(strings.NewReplacer(top...))
+		tagToEnvKeyStack = tagToEnvKeyStack[:len(tagToEnvKeyStack)-1] // pop
+		return v.Unmarshal(&cfg)
+	})
+}
+
+type fieldCallback func(envVarName string, envNamePrefix string, field reflect.StructField, value reflect.Value) error
+type structCallback func(cfg interface{}) error
+
+// iterateFields is a helper function for ReadFrom and WriteConfigDescription.
+// The function traverses config and its nested structs using DFS algorythm, calling fieldCallback for each list field.
+// Before processing a struct the function calls startSructCb, after - finishStructCb
+// A "mapstructure" tag of struct appends envNamePrefix for all internal fields
+func iterateFields(config interface{}, prefix string, startSructCb structCallback, fieldCb fieldCallback, finishStructCb structCallback) error {
 	value := reflect.ValueOf(config)
 	configType := reflect.Indirect(value).Type()
 
@@ -53,26 +127,38 @@ func ReadFromEnv(config interface{}, prefix string) error {
 		return errors.New("Not struct ptr")
 	}
 
+	if startSructCb != nil {
+		if err := startSructCb(config); err != nil {
+			return err
+		}
+	}
+
 	for i := 0; i < configType.NumField(); i++ {
-		tagValue := configType.Field(i).Tag.Get("mapstructure")
+		field := configType.Field(i)
+		tagValue := field.Tag.Get("mapstructure")
 		if field := reflect.Indirect(reflect.Indirect(value).Field(i)); field.Kind() == reflect.Struct {
-			if err := ReadFromEnv(field.Addr().Interface(), addPrefix(tagValue, prefix)); err != nil {
+			if err := iterateFields(field.Addr().Interface(), addPrefix(tagValue, prefix), startSructCb, fieldCb, finishStructCb); err != nil {
 				return err
 			}
 			continue
 		}
 
+		if fieldCb == nil {
+			continue
+		}
+
 		if tagValue == "" {
-			tagValue = strings.ToUpper(configType.Field(i).Name)
+			tagValue = strings.ToUpper(field.Name)
 		}
-		if prefix != "" {
-			tagToEnvKey = append(tagToEnvKey, tagValue, addPrefix(tagValue, prefix))
+
+		if err := fieldCb(tagValue, prefix, field, value.Elem().Field(i)); err != nil {
+			return err
 		}
-		v.BindEnv(tagValue)
+
 	}
 
-	if len(tagToEnvKey) > 0 {
-		v.SetEnvKeyReplacer(strings.NewReplacer(tagToEnvKey...))
+	if finishStructCb != nil {
+		return finishStructCb(config)
 	}
-	return v.Unmarshal(&config)
+	return nil
 }
---
+++ config/format.go
@@ -0,0 +1,93 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+)
+
+const DescriptionTagName = "description"
+
+type Formatter interface {
+	WriteParam(envName, value string, descriptions []string) error
+}
+
+type paramEntry struct {
+	Description []string
+	Value       string
+}
+
+type paramList struct {
+	envToDescription map[string]*paramEntry
+	orderedKeys      []string
+}
+
+func newDescriptionList() *paramList {
+	return &paramList{envToDescription: make(map[string]*paramEntry)}
+}
+
+func (l *paramList) Add(env, value, description string) {
+	if entry, ok := l.envToDescription[env]; !ok {
+		l.orderedKeys = append(l.orderedKeys, env)
+		l.envToDescription[env] = &paramEntry{Description: []string{description}, Value: value}
+	} else {
+		entry.Description = append(entry.Description, description)
+	}
+}
+
+func (l *paramList) WriteTo(formatter Formatter) error {
+	for _, env := range l.orderedKeys {
+		entry := l.envToDescription[env]
+		if err := formatter.WriteParam(env, entry.Value, entry.Description); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func formatValue(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.Slice:
+		if v.Len() == 0 {
+			return ""
+		}
+		var sb strings.Builder
+		for i := 0; i < v.Len(); i++ {
+			sb.WriteString(fmt.Sprintf("%v,", v.Index(i)))
+		}
+		return sb.String()[:sb.Len()-1]
+	default:
+		return fmt.Sprintf("%v", v)
+	}
+}
+
+// WriteConfigDescription writes config description to formatter.
+// It uses "description" tag to fill description column.
+// If there config has fields with the same env name their descriptions are concateneted.
+// if the field tag is "-", the field is always omitted.
+func WriteConfigDescription(config interface{}, envPrefix string, formatter Formatter) error {
+	list := newDescriptionList()
+	if err := iterateFields(config, envPrefix, nil, func(envVarName, envNamePrefix string, field reflect.StructField, value reflect.Value) error {
+		if !field.IsExported() {
+			return nil
+		}
+		if description := field.Tag.Get(DescriptionTagName); description != "-" {
+			list.Add(addPrefix(envVarName, envNamePrefix), formatValue(value), description)
+		}
+		return nil
+	}, nil); err != nil {
+		return err
+	}
+
+	return list.WriteTo(formatter)
+}
+
+func WriteMarkdownDescription(filename string, config interface{}, envPrefix string) error {
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	return WriteConfigDescription(config, envPrefix, NewMarkdownFormatter(file, true))
+}
