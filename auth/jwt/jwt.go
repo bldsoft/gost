d2f21749a6d08734699bff0874c6fbbae066181e
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2022-08-26 17:11:11 +0300

    [STR-1455] changed jwt config

--- auth/jwt/jwt.go
+++ auth/jwt/jwt.go
@@ -7,17 +7,24 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/jwtauth"
+	"github.com/lestrrat-go/jwx/jwa"
 	"github.com/lestrrat-go/jwx/jwk"
+	"github.com/lestrrat-go/jwx/jwt"
 )
 
 type JwtConfig struct {
-	Alg       string `mapstructure:"JWT_ALG" description:"Algorithm used to sign JWT"`
-	PemPath   string `mapstructure:"JWT_PUBLIC_KEY_PATH" description:"Path to PEM encoded ASN.1 DER format key"`
-	publicKey jwk.Key
+	Alg     string `mapstructure:"JWT_ALG" description:"Algorithm used to sign JWT"`
+	PemPath string `mapstructure:"JWT_KEY_PATH" description:"Path to PEM encoded ASN.1 DER format key"`
+	key     jwk.Key
+}
+
+func (c *JwtConfig) PrivateKey() jwk.Key {
+	return c.key
 }
 
 func (c *JwtConfig) PublicKey() jwk.Key {
-	return c.publicKey
+	public, _ := c.key.PublicKey()
+	return public
 }
 
 func (c *JwtConfig) SetDefaults() {}
@@ -26,25 +33,28 @@ func (c *JwtConfig) Validate() (err error) {
 	if len(c.PemPath) != 0 {
 		bytes, err := ioutil.ReadFile(c.PemPath)
 		if err != nil {
-			return fmt.Errorf("failed to read jwt public key: %w", err)
+			return fmt.Errorf("failed to read jwt key: %w", err)
 		}
 
-		c.publicKey, err = jwk.ParseKey(bytes, jwk.WithPEM(true))
+		c.key, err = jwk.ParseKey(bytes, jwk.WithPEM(true))
+
 		if err != nil {
-			return fmt.Errorf("failed to parse jwt public key: %w", err)
+			return fmt.Errorf("failed to parse jwt key: %w", err)
 		}
 	}
 	return nil
 }
 
-// JwtAuthMiddleware accepts either a raw key (e.g. rsa.PrivateKey, ecdsa.PrivateKey, etc)
-// or a jwk.Key, and the name of the algorithm that should be used to sign the token.
 func JwtAuthMiddlewareFromConfig(cfg JwtConfig) func(next http.Handler) http.Handler {
-	return chi.Chain(jwtauth.Verifier(jwtauth.New(cfg.Alg, cfg.publicKey, nil)), jwtauth.Authenticator).Handler
+	return JwtAuthMiddleware(cfg.Alg, cfg.PublicKey())
 }
 
 // JwtAuthMiddleware accepts either a raw key (e.g. rsa.PrivateKey, ecdsa.PrivateKey, etc)
 // or a jwk.Key, and the name of the algorithm that should be used to sign the token.
 func JwtAuthMiddleware(alg string, signKey interface{}) func(next http.Handler) http.Handler {
 	return chi.Chain(jwtauth.Verifier(jwtauth.New(alg, signKey, nil)), jwtauth.Authenticator).Handler
+}
+
+func SignToken(token jwt.Token, cfg JwtConfig) (signed []byte, err error) {
+	return jwt.Sign(token, jwa.SignatureAlgorithm(cfg.Alg), cfg.PrivateKey())
 }
