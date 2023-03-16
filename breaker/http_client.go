7578c1c86d03234b0a6d4caf75029c11117e3676
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2023-03-16 13:38:35 +0300

    [STR-2101] added wrapper for http client with circuit breaker

--- breaker/http_client.go
+++ breaker/http_client.go
@@ -1,7 +1,9 @@
 package breaker
 
 import (
+	"io"
 	"net/http"
+	"net/url"
 )
 
 type Client struct {
@@ -9,26 +11,41 @@ type Client struct {
 	circuitBreaker *CircuitBreaker
 }
 
-// func NewClient(c http.Client) *Client {
-// 	return &Client{Client: c, circuitBreaker: NewCircuitBreaker()}
-// }
+func NewClient(c http.Client, settings settings) *Client {
+	return &Client{Client: c, circuitBreaker: NewCircuitBreaker(settings)}
+}
 
-// func (c *Client) Do(req *http.Request) (*http.Response, error) {
+func (c *Client) Do(req *http.Request) (*http.Response, error) {
+	resp, err := c.circuitBreaker.Execute(func() (interface{}, error) {
+		return c.Client.Do(req)
+	})
+	return resp.(*http.Response), err
+}
 
-// }
+func (c *Client) Get(url string) (*http.Response, error) {
+	resp, err := c.circuitBreaker.Execute(func() (interface{}, error) {
+		return c.Client.Get(url)
+	})
+	return resp.(*http.Response), err
+}
 
-// func (c *Client) Get(url string) (resp *http.Response, err error) {
+func (c *Client) Head(url string) (*http.Response, error) {
+	resp, err := c.circuitBreaker.Execute(func() (interface{}, error) {
+		return c.Client.Head(url)
+	})
+	return resp.(*http.Response), err
+}
 
-// }
+func (c *Client) Post(url string, contentType string, body io.Reader) (*http.Response, error) {
+	resp, err := c.circuitBreaker.Execute(func() (interface{}, error) {
+		return c.Client.Post(url, contentType, body)
+	})
+	return resp.(*http.Response), err
+}
 
-// func (c *Client) Head(url string) (resp *http.Response, err error) {
-
-// }
-
-// func (c *Client) Post(url string, contentType string, body io.Reader) (resp *http.Response, err error) {
-
-// }
-
-// func (c *Client) PostForm(url string, data url.Values) (resp *http.Response, err error) {
-
-// }
+func (c *Client) PostForm(url string, data url.Values) (*http.Response, error) {
+	resp, err := c.circuitBreaker.Execute(func() (interface{}, error) {
+		return c.Client.PostForm(url, data)
+	})
+	return resp.(*http.Response), err
+}
