39b5d495ac67dae6835de94bd8fc7a2107e8d5f1
Arthur D <a.dranchuk@setplex.com>
2023-05-22 16:28:18 +0300

    fix: getobjectfrombody

--- controller/base_controller.go
+++ controller/base_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"encoding/json"
+	"io"
 	"io/ioutil"
 	"net/http"
 
@@ -10,8 +11,7 @@ import (
 
 const ArchivedQueryName = "archived"
 
-type BaseController struct {
-}
+type BaseController struct{}
 
 func (c BaseController) ResponseError(w http.ResponseWriter, err string, code int) {
 	ResponseError(w, err, code)
@@ -23,7 +23,7 @@ func (c BaseController) ResponseOK(w http.ResponseWriter) {
 
 func (c BaseController) ResponseJson(w http.ResponseWriter, r *http.Request, v interface{}, needObjectLog ...bool) {
 	w.Header().Set("Content-Type", "application/json")
-	//err := json.NewEncoder(w).Encode(v)
+	// err := json.NewEncoder(w).Encode(v)
 	body, err := json.Marshal(v)
 	if len(needObjectLog) == 0 || needObjectLog[0] {
 		log.FromContext(r.Context()).DebugOrErrorWithFields(err, log.Fields{"body": string(body), "object": v}, "Encode response json")
@@ -37,12 +37,17 @@ func (c BaseController) ResponseJson(w http.ResponseWriter, r *http.Request, v i
 func (c BaseController) GetObjectFromBody(w http.ResponseWriter, r *http.Request, obj interface{}, needObjectLog ...bool) bool {
 	var bodyBytes []byte
 	if r.Body != nil {
-		bodyBytes, _ = ioutil.ReadAll(r.Body)
+		if contentlen := r.ContentLength; contentlen <= 0 {
+			bodyBytes, _ = ioutil.ReadAll(r.Body)
+		} else {
+			bodyBytes = make([]byte, contentlen)
+			io.ReadFull(r.Body, bodyBytes)
+		}
 		r.Body.Close()
-		//r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+		// r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 	}
 
-	//err := json.NewDecoder(bytes.NewBuffer(bodyBytes)).Decode(obj)
+	// err := json.NewDecoder(bytes.NewBuffer(bodyBytes)).Decode(obj)
 	err := json.Unmarshal(bodyBytes, obj)
 	if len(needObjectLog) == 0 || needObjectLog[0] {
 		log.FromContext(r.Context()).DebugOrErrorWithFields(err, log.Fields{"body": string(bodyBytes), "object": obj}, "Decode request json")
