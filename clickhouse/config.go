02040ce9153eb0538a19cdd8178fcecc2a2858f0
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2023-05-23 16:36:54 +0300

    [STR-1623] convert v1 client DSN to v2

--- clickhouse/config.go
+++ clickhouse/config.go
@@ -1,6 +1,8 @@
 package clickhouse
 
 import (
+	"net/url"
+
 	"github.com/ClickHouse/clickhouse-go/v2"
 	gost "github.com/bldsoft/gost/config"
 )
@@ -17,6 +19,31 @@ func (c *Config) SetDefaults() {
 
 // Validate ...
 func (c *Config) Validate() (err error) {
+	if err := c.prepareDsn(); err != nil {
+		return err
+	}
+
 	c.options, err = clickhouse.ParseDSN(c.Dsn.String())
 	return err
+}
+
+// convert v1 client DSN to v2
+func (c *Config) prepareDsn() error {
+	url, err := url.Parse(c.Dsn.String())
+	if err != nil {
+		return err
+	}
+	if url.Scheme != "clickhouse" {
+		url.Scheme = "clickhouse"
+
+		// remove default database from query and set it to path
+		database := url.Query().Get("database")
+		query := url.Query()
+		query.Del("database")
+		url.RawQuery = query.Encode()
+		url.Path = "/" + database
+
+		c.Dsn = gost.ConnectionString(url.String())
+	}
+	return nil
 }
