92a6ff4e07524cb5ab3c83d520f05d0c2bb2f6bb
Arthur D <a.dranchuk@setplex.com>
2023-05-26 14:51:34 +0300

    fix: note comment

--- mongo/interface.go
+++ mongo/interface.go
@@ -8,10 +8,10 @@ import (
 )
 
 // NOTE: I used golang/mock@v1.7.0-rc.1 to generate mocks for mongo
-// the probleim is it treated T withing repository.IEntityIDPtr as a custom type from this package
-// meaning, it generated the following signature for mock:
+// the probleim is it treats `T` withing `repository.IEntityIDPtr` as a custom type from this package
+// meaning, it generates the following signature for mock:
 // MockRepository[T any, U repository.IEntityIDPtr[mongo.T]] struct
-// if regeneration is neaded, make sure to replace it with
+// if regeneration is neaded, make sure to replace the resulting signature with
 // MockRepository[T any, U repository.IEntityIDPtr[T]] struct
 type Repository[T any, U repository.IEntityIDPtr[T]] interface {
 	Name() string
