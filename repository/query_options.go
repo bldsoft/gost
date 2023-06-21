80281c4ec4fc384b829c011a9c3bacc25117f814
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2023-06-21 11:47:58 +0300

    [STR-2257] added sort option for mongo repository

--- repository/query_options.go
+++ repository/query_options.go
@@ -3,5 +3,23 @@ package repository
 type QueryOptions struct {
 	Archived bool
 	Fields   []string // option for read operations, empty slice means all
+	Sort     SortOpt
 	Filter   interface{}
+}
+
+type SortField struct {
+	Field string
+	Desc  bool
+}
+
+type SortOpt []SortField
+
+func Sort() SortOpt { return nil }
+
+func (o SortOpt) Asc(field string) SortOpt {
+	return append(o, SortField{Field: field})
+}
+
+func (o SortOpt) Desc(field string) SortOpt {
+	return append(o, SortField{Field: field, Desc: true})
 }
