be5e48f9cfd46dbadcf32819ada4dfbfdcc7c6f2
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2023-10-12 10:17:47 +0300

    [STR-2538] changed repository watcher interface

--- mongo/interface.go
+++ mongo/interface.go
@@ -8,7 +8,7 @@ import (
 )
 
 // NOTE: I used golang/mock@v1.7.0-rc.1 to generate mocks for mongo
-// the probleim is it treats `T` withing `repository.IEntityIDPtr` as a custom type from this package
+// the probleim is it treats T withing repository.IEntityIDPtr as a custom type from this package
 // meaning, it generates the following signature for mock:
 // MockRepository[T any, U repository.IEntityIDPtr[mongo.T]] struct
 // if regeneration is neaded, make sure to replace the resulting signature with
@@ -18,23 +18,7 @@ type Repository[T any, U repository.IEntityIDPtr[T]] interface {
 	Collection() *mongo.Collection
 	WithTransaction(ctx context.Context, f func(ctx mongo.SessionContext) (interface{}, error)) (interface{}, error)
 
-	FindOne(ctx context.Context, filter interface{}, opts ...*repository.QueryOptions) (U, error)
-	FindByID(ctx context.Context, id interface{}, options ...*repository.QueryOptions) (U, error)
-	FindByStringIDs(ctx context.Context, ids []string, preserveOrder bool, options ...*repository.QueryOptions) ([]U, error)
-	FindByIDs(ctx context.Context, ids []interface{}, preserveOrder bool, options ...*repository.QueryOptions) ([]U, error)
-	Find(ctx context.Context, filter interface{}, opt ...*repository.QueryOptions) ([]U, error)
-	GetAll(ctx context.Context, options ...*repository.QueryOptions) ([]U, error)
-
-	Insert(ctx context.Context, entity U) error
-	InsertMany(ctx context.Context, entities []U) error
-	Update(ctx context.Context, entity U, options ...*repository.QueryOptions) error
-	UpdateMany(ctx context.Context, entities []U) error
-	UpdateOne(ctx context.Context, filter interface{}, update interface{}, options ...*repository.QueryOptions) error
-	UpdateAndGetByID(ctx context.Context, updateEntity U, returnNewDocument bool, queryOpt ...*repository.QueryOptions) (U, error)
-	Upsert(ctx context.Context, entity U, opt ...*repository.QueryOptions) error
-	UpsertOne(ctx context.Context, filter interface{}, update U) error
-	Delete(ctx context.Context, id interface{}, options ...*repository.QueryOptions) error
-	DeleteMany(ctx context.Context, filter interface{}, options ...*repository.QueryOptions) error
+	repository.Repository[T, U]
 
 	AggregateOne(ctx context.Context, pipeline mongo.Pipeline, entity interface{}) error
 }
---
+++ repository/interface.go
@@ -0,0 +1,73 @@
+package repository
+
+import (
+	"context"
+)
+
+type Repository[T any, U IEntityIDPtr[T]] interface {
+	FindOne(ctx context.Context, filter interface{}, opts ...*QueryOptions) (U, error)
+	FindByID(ctx context.Context, id interface{}, options ...*QueryOptions) (U, error)
+	FindByStringIDs(ctx context.Context, ids []string, preserveOrder bool, options ...*QueryOptions) ([]U, error)
+	FindByIDs(ctx context.Context, ids []interface{}, preserveOrder bool, options ...*QueryOptions) ([]U, error)
+	Find(ctx context.Context, filter interface{}, opt ...*QueryOptions) ([]U, error)
+	GetAll(ctx context.Context, options ...*QueryOptions) ([]U, error)
+
+	Insert(ctx context.Context, entity U) error
+	InsertMany(ctx context.Context, entities []U) error
+	Update(ctx context.Context, entity U, options ...*QueryOptions) error
+	UpdateMany(ctx context.Context, entities []U) error
+	UpdateOne(ctx context.Context, filter interface{}, update interface{}, options ...*QueryOptions) error
+	UpdateAndGetByID(ctx context.Context, updateEntity U, returnNewDocument bool, queryOpt ...*QueryOptions) (U, error)
+	Upsert(ctx context.Context, entity U, opt ...*QueryOptions) error
+	UpsertOne(ctx context.Context, filter interface{}, update U) error
+	Delete(ctx context.Context, id interface{}, options ...*QueryOptions) error
+	DeleteMany(ctx context.Context, filter interface{}, options ...*QueryOptions) error
+}
+
+//go:generate go run github.com/abice/go-enum -f=$GOFILE
+
+// ENUM(create, update, delete)
+type EventType string
+
+type Event[T any, U IEntityIDPtr[T]] struct {
+	Entity U
+	Type   EventType
+}
+
+type Watcher[T any, U IEntityIDPtr[T]] interface {
+	WarmUp(ctx context.Context, rep Repository[T, U]) error
+	OnEvent(event Event[T, U])
+}
+
+type WatchedRepository[T any, U IEntityIDPtr[T]] interface {
+	Repository[T, U]
+	AddWatcher(w Watcher[T, U])
+}
+
+func NewWatcher[T any, U IEntityIDPtr[T]](
+	warmUp func(ctx context.Context, rep Repository[T, U]) error,
+	onEvent func(event Event[T, U]),
+) Watcher[T, U] {
+	return watcher[T, U]{
+		warmUp:  warmUp,
+		onEvent: onEvent,
+	}
+}
+
+type watcher[T any, U IEntityIDPtr[T]] struct {
+	warmUp  func(ctx context.Context, rep Repository[T, U]) error
+	onEvent func(event Event[T, U])
+}
+
+func (w watcher[T, U]) WarmUp(ctx context.Context, rep Repository[T, U]) error {
+	if w.warmUp != nil {
+		return w.warmUp(ctx, rep)
+	}
+	return nil
+}
+
+func (w watcher[T, U]) OnEvent(event Event[T, U]) {
+	if w.onEvent != nil {
+		w.onEvent(event)
+	}
+}
