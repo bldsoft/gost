66b71ea4151061328b650a5540bbb5d4bc352799
Arthur D <a.dranchuk@setplex.com>
2023-10-25 13:51:13 +0300

    fix: bring back origin get method

--- log/request_info.go
+++ log/request_info.go
@@ -61,6 +61,8 @@ func GetRequestMethodType(type_str string) RequestMethodType {
 		return HEAD
 	case "PATCH":
 		return PATCH
+	case "ORIGIN_GET":
+		return ORIGIN_GET
 	default:
 		return ERROR
 	}
