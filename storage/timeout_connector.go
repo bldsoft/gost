cc56f3b183f84ea74aba3c7f8ff41dff9efa9a3f
Arthur D <a.dranchuk@setplex.com>
2023-11-23 20:19:23 +0300

    fix: use err group instead of wg

--- storage/timeout_connector.go
+++ storage/timeout_connector.go
@@ -2,34 +2,35 @@ package storage
 
 import (
 	"fmt"
-	"sync"
 	"time"
 
 	"github.com/bldsoft/gost/log"
+	"github.com/bldsoft/gost/utils/errgroup"
 )
 
-func DBConnectAsync(wg *sync.WaitGroup, connect func(), n int, sleepPeriod time.Duration) {
-	wg.Add(1)
+func DBConnectAsync(eg *errgroup.Group, connect func(), n int, sleepPeriod time.Duration) {
 	go func() {
-		defer wg.Done()
-		for i := 1; (i != n+1) || (n < 0); i++ {
-			err := func() (err error) {
-				defer func() {
-					if _err := recover(); _err != nil {
-						err = fmt.Errorf("%v", err)
-					}
+		eg.Go(func() error {
+			var err error
+			for i := 1; (i != n+1) || (n < 0); i++ {
+				err = func() (err error) {
+					defer func() {
+						if _err := recover(); _err != nil {
+							err = fmt.Errorf("%v", err)
+						}
+					}()
+					connect()
+					return
 				}()
-				connect()
-				return
-			}()
+				if err != nil {
+					log.ErrorWithFields(log.Fields{"error": err}, "error connecting to db")
+					time.Sleep(sleepPeriod)
+					continue
+				}
 
-			if err != nil {
-				log.ErrorWithFields(log.Fields{"error": err}, "error connecting to db")
-				time.Sleep(sleepPeriod)
-				continue
+				return nil
 			}
-
-			return
-		}
+			return err
+		})
 	}()
 }
