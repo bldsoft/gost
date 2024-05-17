37edee17583c0e2a69c48b52a6c342f8041511dc
Arthur D <a.dranchuk@setplex.com>
2024-05-17 19:53:59 +0300

    fix: generic obfuscator

--- discovery/inhouse/config.go
+++ discovery/inhouse/config.go
@@ -9,11 +9,11 @@ import (
 )
 
 type Config struct {
-	Embedded               bool                `mapstructure:"EMBEDDED" description:"If true, in-house discovery will use service bind address"`
-	BindAddress            config.Address      `mapstructure:"BIND_ADDRESS" description:"For non embedded mode. Configuration related to what address to bind to and ports to listen on."`
-	ClusterMembers         []string            `mapstructure:"CLUSTER_MEMBERS" description:"Comma separated list of any existing member of the cluster to join it. Example: '127.0.0.1:3001'"`
-	SecretKey              config.HiddenString `mapstructure:"SECRET_KEY" description:"SecretKey is used to encrypt messages. The value should be either 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256."`
-	DeregisterServiceAfter time.Duration       `mapstructure:"DEREGISTER_SERVICE_AFTER" description:"The interval after which the downed service is removed from the cluster"`
+	Embedded               bool              `mapstructure:"EMBEDDED" description:"If true, in-house discovery will use service bind address"`
+	BindAddress            config.Address    `mapstructure:"BIND_ADDRESS" description:"For non embedded mode. Configuration related to what address to bind to and ports to listen on."`
+	ClusterMembers         []string          `mapstructure:"CLUSTER_MEMBERS" description:"Comma separated list of any existing member of the cluster to join it. Example: '127.0.0.1:3001'"`
+	SecretKey              utils.FullyHidden `mapstructure:"SECRET_KEY" description:"SecretKey is used to encrypt messages. The value should be either 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256."`
+	DeregisterServiceAfter time.Duration     `mapstructure:"DEREGISTER_SERVICE_AFTER" description:"The interval after which the downed service is removed from the cluster"`
 }
 
 func (c *Config) SetDefaults() {
