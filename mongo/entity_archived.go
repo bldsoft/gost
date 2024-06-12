82e0e343304dd88fdb802241fc175eab85fc4550
Arthur D <a.dranchuk@setplex.com>
2024-06-12 19:19:01 +0300

    fix: cache find by id w archived

--- mongo/entity_archived.go
+++ mongo/entity_archived.go
@@ -4,6 +4,13 @@ const (
 	BsonFieldNameArchived = "archived"
 )
 
+type WithEntityArchived interface {
+	IsArchived() bool
+}
 type EntityArchived struct {
 	Archived bool `json:"archived,omitempty" bson:"archived,omitempty"`
+}
+
+func (e EntityArchived) IsArchived() bool {
+	return e.Archived
 }
