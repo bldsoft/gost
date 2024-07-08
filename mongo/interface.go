2e08182e1ebec03e472fd1c3aa8552931390628a
Arthur D <a.dranchuk@setplex.com>
2024-07-08 13:27:19 +0300

    fix: mongo mocks

--- mongo/interface.go
+++ mongo/interface.go
@@ -7,12 +7,8 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-// NOTE: I used golang/mock@v1.7.0-rc.1 to generate mocks for mongo
-// the probleim is it treats T withing repository.IEntityIDPtr as a custom type from this package
-// meaning, it generates the following signature for mock:
-// MockRepository[T any, U repository.IEntityIDPtr[mongo.T]] struct
-// if regeneration is neaded, make sure to replace the resulting signature with
-// MockRepository[T any, U repository.IEntityIDPtr[T]] struct
+//go:generate go run github.com/vektra/mockery/v2 --all --with-expecter
+
 type Repository[T any, U repository.IEntityIDPtr[T]] interface {
 	Name() string
 	Collection() *mongo.Collection
