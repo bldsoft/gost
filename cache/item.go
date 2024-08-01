c8f2d11de7d05b737ca54a0ddbb0e781b1d8e69f
Arthur D <a.dranchuk@setplex.com>
2024-08-01 11:18:11 +0300

    fix: rmv deadline

--- cache/item.go
+++ cache/item.go
@@ -3,9 +3,10 @@ package cache
 import "time"
 
 type Item struct {
-	Value []byte
-	TTL   time.Duration
-	Flags uint32
+	Value    []byte
+	TTL      time.Duration
+	Flags    uint32
+	Deadline time.Time
 }
 
 type ItemF func(*Item)
