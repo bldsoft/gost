ff150b1c8bbbb8df72110369db16ccfffaf5503e
Arthur D <a.dranchuk@setplex.com>
2024-10-22 17:29:07 +0300

    fix: v2 package

--- cacheV2/bigcache/expiring_repository.go
+++
@@ -1,7 +0,0 @@
-package bigcache
-
-import "github.com/bldsoft/gost/cache"
-
-func NewExpiringRepository(jsonConfig string) *cache.ExpiringCacheRepository {
-	return cache.NewExpiringRepository(NewRepository(jsonConfig))
-}
