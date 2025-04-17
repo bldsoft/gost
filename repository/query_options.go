caff89425344491d29ccb86f5011ad08aa6e126b
Arthur D <a.dranchuk@setplex.com>
2025-04-17 13:31:44 +0300

    fix: rmv modifyoptions

--- repository/query_options.go
+++ repository/query_options.go
@@ -1,7 +1,5 @@
 package repository
 
-import "go.mongodb.org/mongo-driver/bson"
-
 type QueryOptions struct {
 	Archived bool
 	Fields   []string // option for read operations, empty slice means all
@@ -26,21 +24,4 @@ func (o SortOpt) Asc(field string) SortOpt {
 
 func (o SortOpt) Desc(field string) SortOpt {
 	return append(o, SortField{Field: field, Desc: true})
-}
-
-type ModifyOptions struct {
-	DuplicateFilter interface{}
-	QueryOptions
-}
-
-func BuildDuplicateFilter[T any](fieldName string, f func(t T) interface{}, vals ...T) *ModifyOptions {
-	if len(vals) == 0 {
-		return nil
-	}
-
-	filter := make([]any, 0, len(vals))
-	for _, val := range vals {
-		filter = append(filter, f(val))
-	}
-	return &ModifyOptions{DuplicateFilter: bson.M{fieldName: bson.M{"$in": filter}}}
 }
