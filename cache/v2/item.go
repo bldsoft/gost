11a0581a64e6077fb2af65092f9d5b984e3ae6e7
Arthur D <a.dranchuk@setplex.com>
2025-07-11 16:55:32 +0300

    WIP: extand bins

--- cache/v2/item.go
+++ cache/v2/item.go
@@ -3,9 +3,10 @@ package cache
 import "time"
 
 type Item struct {
-	Value []byte
-	TTL   time.Duration
-	Flags uint32
+	Value  []byte
+	TTL    time.Duration
+	Flags  uint32
+	Extras map[string]any
 }
 
 type ItemF func(*Item)
@@ -27,5 +28,14 @@ func WithTTL(ttl time.Duration) ItemF {
 func WithFlags(flags uint32) ItemF {
 	return func(it *Item) {
 		it.Flags = flags
+	}
+}
+
+func WithExtras(key string, value any) ItemF {
+	return func(it *Item) {
+		if it.Extras == nil {
+			it.Extras = make(map[string]any)
+		}
+		it.Extras[key] = value
 	}
 }
