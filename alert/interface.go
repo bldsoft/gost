2d02ed448743f57a63d522533ec14e2634346b6c
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2025-09-23 18:35:26 +0300

    [SPL-1150] alert interfaces

---
+++ alert/interface.go
@@ -0,0 +1,61 @@
+package alert
+
+import (
+	"context"
+	"slices"
+	"time"
+)
+
+type SeverityLevel int
+
+const (
+	SeverityLow SeverityLevel = iota
+	SeverityMedium
+	SeverityHigh
+	SeverityCritical
+)
+
+type Alert struct {
+	SourceID string
+
+	Severity  SeverityLevel
+	From, To  time.Time
+	Notifiers []Notifier
+
+	MetaData map[string]string
+}
+
+type Handler interface {
+	Handle(ctx context.Context, alerts ...Alert)
+}
+
+type Middleware func(Handler) Handler
+
+type AlertHandlerFunc func(ctx context.Context, alerts ...Alert)
+
+func (f AlertHandlerFunc) Handle(ctx context.Context, alerts ...Alert) {
+	f(ctx, alerts...)
+}
+
+func Middlewares(middlewares ...Middleware) Middleware {
+	return func(next Handler) Handler {
+		for _, m := range slices.Backward(middlewares) {
+			next = m(next)
+		}
+		return next
+	}
+}
+
+type Notifier interface {
+	Send(ctx context.Context, alerts Alert) error
+}
+
+type Source interface {
+	EvaluateAlerts(ctx context.Context) (alerts []Alert, next time.Time, err error)
+}
+
+type SourceFunc func(ctx context.Context) ([]Alert, time.Time, error)
+
+func (f SourceFunc) EvaluateAlerts(ctx context.Context) ([]Alert, time.Time, error) {
+	return f(ctx)
+}
