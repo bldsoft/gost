873b22a20b3830a6b5fdc9f390c9829df2598a41
Matvei M <m.mirashnichenka@setplex.com>
2025-09-30 18:33:52 +0300

    draft tests and middlewares

--- alert/middleware.go
+++ alert/middleware.go
@@ -2,48 +2,70 @@ package alert
 
 import (
 	"context"
+	"errors"
 	"fmt"
-	"log"
 	"time"
 
 	"github.com/bldsoft/gost/cache"
+	"github.com/bldsoft/gost/log"
 )
 
-func FilterBySeverityMiddleware(minSeverity SeverityLevel) Middleware {
+func GroupRecurringMiddleware(store cache.Repository[time.Time], period time.Duration) Middleware {
 	return func(next Handler) Handler {
 		return AlertHandlerFunc(func(ctx context.Context, alerts ...Alert) {
-			var filtered []Alert
+			var grouped []Alert
+
 			for _, alert := range alerts {
-				if alert.Severity >= minSeverity {
-					filtered = append(filtered, alert)
+				to, err := store.Get(alert.SourceID)
+				if err == nil {
+					if to.After(time.Now()) {
+						continue
+					}
+				} else if !errors.Is(err, cache.ErrCacheMiss) {
+					log.FromContext(ctx).ErrorWithFields(log.Fields{"err": err}, "alerts reccuring middleware: failed to get value from store")
 				}
+
+				if store.SetFor(alert.SourceID, time.Now().Add(period), period) != nil {
+					log.FromContext(ctx).ErrorWithFields(log.Fields{"err": err}, "alerts reccuring middleware: failed to store alert")
+				}
+
+				grouped = append(grouped, alert)
 			}
-			if len(filtered) > 0 {
-				next.Handle(ctx, filtered...)
-			}
+
+			next.Handle(ctx, grouped...)
 		})
 	}
 }
 
+// TODO: handle restart cases (when we get previous alerts that were completely sent)
 func DeduplicationMiddleware(store cache.Repository[Alert], ttl time.Duration) Middleware {
 	return func(next Handler) Handler {
 		return AlertHandlerFunc(func(ctx context.Context, alerts ...Alert) {
 			var deduplicated []Alert
 			for _, alert := range alerts {
-				uniqueKey := fmt.Sprintf("%s:%d:%d", alert.SourceID, alert.From.Unix(), alert.To.Unix())
-				if _, err := store.Get(uniqueKey); err != cache.ErrCacheMiss { //TODO: are we always hoping for cache miss?
-					continue
+				key := fmt.Sprintf("%s-%d", alert.SourceID, alert.Severity)
+
+				_, err := store.Get(key)
+
+				if alert.To.IsZero() {
+					if err == nil {
+						continue //if we have this alert in store it means that we didnt receive finishing for this alert
+					} else {
+						if store.SetFor(key, alert, ttl) != nil {
+							fmt.Println("failed to set cache alert with fast key ", key, " err ", err)
+						}
+					}
+				} else {
+					if err == nil {
+						if store.Delete(key) != nil {
+							fmt.Println("failed to delete cache alert with fast key ", key, " err ", err)
+						} //we received finishing for this alert and can delete it from store
+					} else {
+						continue //weve already received finisher for this alert
+					}
 				}
 
 				deduplicated = append(deduplicated, alert)
-				err := store.SetFor(uniqueKey, alert, ttl)
-				if err != nil {
-					log.Printf("Failed to set alert in store: %v", err)
-				}
-			}
-
-			if len(deduplicated) == 0 {
-				return
 			}
 
 			next.Handle(ctx, deduplicated...)
