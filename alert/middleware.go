c4e51d89fd69a8d805a55a981bb5366307e0f8d9
Matvei M <m.mirashnichenka@setplex.com>
2025-10-09 17:26:01 +0300

    WIP

--- alert/middleware.go
+++ alert/middleware.go
@@ -16,17 +16,19 @@ func GroupRecurringMiddleware(store cache.Repository[time.Time], period time.Dur
 			grouped := make([]Alert, 0, len(alerts))
 
 			for _, alert := range alerts {
-				to, err := store.Get(alert.SourceID)
+				key := fmt.Sprintf("group-%s-%d", alert.SourceID, alert.Severity)
+				to, err := store.Get(key)
 				if err != nil && !errors.Is(err, cache.ErrCacheMiss) {
 					log.FromContext(ctx).ErrorWithFields(log.Fields{"err": err}, "alerts recurring middleware: failed to get value from store")
 				}
+				now := time.Now()
 
 				// Skip if alert is still within cooldown period
-				if err == nil && to.After(time.Now()) {
+				if err == nil && to.After(now) {
 					continue
 				}
 
-				if err := store.SetFor(alert.SourceID, time.Now().Add(period), period); err != nil {
+				if err := store.SetFor(key, now.Add(period), period); err != nil {
 					log.FromContext(ctx).ErrorWithFields(log.Fields{"err": err}, "alerts recurring middleware: failed to store alert")
 				}
 
@@ -38,7 +40,7 @@ func GroupRecurringMiddleware(store cache.Repository[time.Time], period time.Dur
 	}
 }
 
-// DeduplicationMiddleware TTL is being used for caching alerts to prevent duplicates on sender restarts
+// DeduplicationMiddleware TTL is being used for caching alerts to prevent duplicates on sender restarts etc
 func DeduplicationMiddleware(store cache.Repository[Alert], TTL time.Duration) Middleware {
 	return func(next Handler) Handler {
 		return AlertHandlerFunc(func(ctx context.Context, alerts ...Alert) {
@@ -47,10 +49,10 @@ func DeduplicationMiddleware(store cache.Repository[Alert], TTL time.Duration) M
 
 			for _, alert := range alerts {
 				//for alerts where we are waiting for the finisher
-				key := fmt.Sprintf("%s-%d", alert.SourceID, alert.Severity)
+				key := fmt.Sprintf("dedup-%s-%d-wait", alert.SourceID, alert.Severity)
 
 				//for alerts that were already finished (in case of sender restarts etc.)
-				timedKey := fmt.Sprintf("%s-%d-recvd", alert.SourceID, alert.Severity)
+				timedKey := fmt.Sprintf("dedup-%s-%d-recvd", alert.SourceID, alert.Severity)
 				if val, err := store.Get(timedKey); err == nil {
 					if val.To.After(alert.From) {
 						continue //already received this alert
@@ -61,30 +63,25 @@ func DeduplicationMiddleware(store cache.Repository[Alert], TTL time.Duration) M
 				alertExistsInStore := err == nil
 				isStartAlert := alert.To.IsZero()
 
-				// Handle start alert
 				if isStartAlert {
 					if alertExistsInStore {
-						continue // Already received this start alert
+						continue // already received this start alert
 					}
 					if err := store.Set(key, alert); err != nil {
-						logger.ErrorWithFields(log.Fields{"err": err, "key": key}, "alerts deduplication middleware: failed to store alert in cache")
+						logger.ErrorWithFields(log.Fields{"err": err, "key": key}, "alerts deduplication middleware: failed to store alert in cache with normal key")
 					}
 					deduplicated = append(deduplicated, alert)
 					continue
 				}
 
-				// Handle end alert
-				if !alertExistsInStore {
-					continue // Already received the end alert for this
-				}
-
-				if err := store.Delete(key); err != nil {
-					logger.ErrorWithFields(log.Fields{"err": err, "key": key}, "alerts deduplication middleware: failed to delete alert from cache")
-				}
+				//either delete entry if this is an end alert or fail if this
+				// is "happened" (started in finished in the same window) alert
+				// that we didn't store, we don't care
+				_ = store.Delete(key)
 
 				// Store in case we get this alert again (e.g. sender restarts)
 				if err := store.SetFor(timedKey, alert, TTL); err != nil {
-					logger.ErrorWithFields(log.Fields{"err": err, "key": timedKey}, "alerts deduplication middleware: failed to store received end alert in cache")
+					logger.ErrorWithFields(log.Fields{"err": err, "key": timedKey}, "alerts deduplication middleware: failed to store received alert in cache with timed key")
 				}
 
 				deduplicated = append(deduplicated, alert)
