36f978193abd3685f670661a50fdf4ec3ef739e4
Vladislav Krivoguzov <u.kryvahuzau@setplex.com>
2025-10-17 12:35:07 +0300

    [SPL-1150] draft alert queue

---
+++ alert/queue.go
@@ -0,0 +1,152 @@
+package alert
+
+import (
+	"context"
+	"iter"
+	"sync"
+	"time"
+
+	"github.com/bldsoft/gost/utils/heap"
+)
+
+type item[T any] struct {
+	value T
+	next  time.Time
+}
+
+type queue[T any] struct {
+	heap   *heap.Heap[item[T]]
+	mtx    *sync.RWMutex
+	pushed chan struct{}
+	closed bool
+}
+
+func newQueue[T any]() *queue[T] {
+	var mtx sync.RWMutex
+	return &queue[T]{
+		heap: heap.NewHeap(func(a, b item[T]) bool {
+			return a.next.Before(b.next)
+		}),
+		mtx:    &mtx,
+		pushed: make(chan struct{}, 1),
+		closed: false,
+	}
+}
+
+func (q *queue[T]) Push(value T, next time.Time) {
+	q.mtx.Lock()
+	defer q.mtx.Unlock()
+	if q.closed {
+		return
+	}
+	q.heap.Push(item[T]{value, next})
+	select {
+	case q.pushed <- struct{}{}:
+	default:
+	}
+}
+
+func (q *queue[T]) popAndWait(ctx context.Context) *item[T] {
+	for {
+		v := q.topWait(ctx)
+		if v == nil {
+			return nil // closed
+		}
+
+		select {
+		case t := <-time.After(time.Until(v.next)):
+			res := func() *item[T] {
+				q.mtx.Lock()
+				defer q.mtx.Unlock()
+				if q.heap.Empty() {
+					return nil
+				}
+				res := q.heap.Top()
+				if !res.next.After(t) {
+					_ = q.heap.Pop()
+					return &res
+				}
+				return nil
+			}()
+			if res != nil {
+				return res
+			}
+		case <-q.pushed:
+		case <-ctx.Done():
+			return nil
+		}
+	}
+}
+
+func (q *queue[T]) RemoveFirstFunc(f func(value T) bool) {
+	q.mtx.Lock()
+	defer q.mtx.Unlock()
+	q.heap.RemoveFirstFunc(func(item item[T]) bool {
+		return f(item.value)
+	})
+	select {
+	case q.pushed <- struct{}{}:
+	default:
+	}
+}
+
+func (q *queue[T]) topWait(ctx context.Context) *item[T] {
+	item, closed := q.top()
+	for item == nil {
+		if closed {
+			return nil
+		}
+		select {
+		case <-q.pushed:
+		case <-ctx.Done():
+			return nil
+		}
+		item, closed = q.top()
+	}
+	return item
+}
+
+func (q *queue[T]) top() (item *item[T], closed bool) {
+	q.mtx.RLock()
+	defer q.mtx.RUnlock()
+	if q.heap.Empty() {
+		return nil, q.closed
+	}
+	res := q.heap.Top()
+	return &res, false
+}
+
+func (q *queue[T]) SyncSeq(ctx context.Context) iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for item := q.popAndWait(ctx); item != nil; item = q.popAndWait(ctx) {
+			if !yield(item.value) {
+				return
+			}
+		}
+	}
+}
+
+func (q *queue[T]) SyncSeq2(ctx context.Context) iter.Seq2[int, T] {
+	return func(yield func(int, T) bool) {
+		i := 0
+		for item := range q.SyncSeq(ctx) {
+			if !yield(i, item) {
+				return
+			}
+			i++
+		}
+	}
+}
+
+func (q *queue[T]) Close() {
+	q.mtx.Lock()
+	defer q.mtx.Unlock()
+	if q.closed {
+		return
+	}
+	q.closed = true
+	select {
+	case q.pushed <- struct{}{}:
+	default:
+	}
+}
---
+++ alert/queue_test.go
@@ -0,0 +1,150 @@
+package alert
+
+import (
+	"cmp"
+	"context"
+	"slices"
+	"sort"
+	"testing"
+	"testing/synctest"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestQueueScheduledTask(t *testing.T) {
+	type producedTaks struct {
+		id              int
+		startIn         time.Duration
+		issuedIn        time.Duration
+		expectedStartIn time.Duration
+	}
+
+	tests := []struct {
+		name         string
+		producedTaks []producedTaks
+	}{
+		{
+			name:         "empty",
+			producedTaks: []producedTaks{},
+		},
+		{
+			name: "normal",
+			producedTaks: []producedTaks{
+				{id: 1, startIn: 10 * time.Second, issuedIn: 0},
+				{id: 2, startIn: 20 * time.Second, issuedIn: 0},
+				{id: 3, startIn: 30 * time.Second, issuedIn: 0},
+			},
+		}, {
+			name: "earlier start, issued later",
+			producedTaks: []producedTaks{
+				{id: 3, startIn: 30 * time.Second, issuedIn: 0},
+				{id: 2, startIn: 20 * time.Second, issuedIn: 0},
+				{id: 1, startIn: 10 * time.Second, issuedIn: 0},
+			},
+		}, {
+			name: "earlier start, issued later",
+			producedTaks: []producedTaks{
+				{id: 1, startIn: time.Second, issuedIn: time.Second},
+				{id: 2, startIn: time.Second * 2, issuedIn: 0},
+			},
+		}, {
+			name: "negative start",
+			producedTaks: []producedTaks{
+				{id: 1, startIn: 10 * time.Second, issuedIn: 0},
+				{id: 2, startIn: -10 * time.Second, issuedIn: 50 * time.Second, expectedStartIn: 50 * time.Second},
+				{id: 3, startIn: 60 * time.Second, issuedIn: 0},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			synctest.Test(t, func(t *testing.T) {
+				queue := newQueue[int]()
+
+				expected := slices.Clone(test.producedTaks)
+
+				sort.Slice(expected, func(i, j int) bool {
+					expectedStartIn := cmp.Or(expected[i].expectedStartIn, expected[i].startIn)
+					expectedStartIn2 := cmp.Or(expected[j].expectedStartIn, expected[j].startIn)
+					return expectedStartIn < expectedStartIn2
+				})
+
+				now := time.Now()
+				go func() {
+					sort.Slice(test.producedTaks, func(i, j int) bool {
+						return test.producedTaks[i].issuedIn < test.producedTaks[j].issuedIn
+					})
+					for _, task := range test.producedTaks {
+						time.Sleep(time.Until(now.Add(task.issuedIn)))
+						queue.Push(task.id, now.Add(task.startIn))
+					}
+					queue.Close()
+				}()
+
+				for i, task := range queue.SyncSeq2(context.Background()) {
+					require.Equal(t, expected[i].id, task)
+					expectedStartIn := cmp.Or(expected[i].expectedStartIn, expected[i].startIn)
+					deviation := time.Since(now.Add(expectedStartIn))
+					deviation = max(deviation, -deviation) // abs
+					require.Less(t, deviation, 10*time.Millisecond, "task %d", task)
+				}
+			})
+		})
+	}
+}
+
+func TestQueueClose(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		queue := newQueue[int]()
+		expected := make([]int, 0, 10)
+		for i := range cap(expected) {
+			expected = append(expected, i)
+			queue.Push(i, time.Now().Add(time.Duration(i)*time.Second))
+		}
+		queue.Close()
+
+		actual := slices.Collect(queue.SyncSeq(context.Background()))
+		require.Equal(t, expected, actual)
+	})
+}
+
+func TestQueueContextDone(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		queue := newQueue[int]()
+		now := time.Now()
+		queue.Push(1, now.Add(time.Second))
+		queue.Push(2, now.Add(2*time.Second))
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+		queue.Push(4, now.Add(4*time.Second))
+		queue.Push(5, now.Add(5*time.Second))
+		actual := slices.Collect(queue.SyncSeq(ctx))
+		require.Equal(t, []int{1, 2}, actual)
+	})
+}
+
+func TestQueueRemoveFunc(t *testing.T) {
+	synctest.Test(t, func(t *testing.T) {
+		queue := newQueue[int]()
+		queue.Push(1, time.Now().Add(time.Second))
+		queue.Push(2, time.Now().Add(2*time.Second))
+		queue.Push(3, time.Now().Add(3*time.Second))
+		queue.Push(4, time.Now().Add(4*time.Second))
+
+		go func() {
+			time.Sleep(1500 * time.Millisecond)
+			for _, v := range []int{2, 4} {
+				queue.RemoveFirstFunc(func(i int) bool {
+					return i == v
+				})
+			}
+			queue.Close()
+		}()
+
+		actual := slices.Collect(queue.SyncSeq(context.Background()))
+		require.Equal(t, []int{1, 3}, actual)
+	})
+}
