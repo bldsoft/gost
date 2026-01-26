7686bbc1fc43fa0d264fa91c1d763ac39c323806
Matvei M <m.mirashnichenka@setplex.com>
2026-01-26 11:27:39 +0300

    add color template for slack

--- alert/notify/channel/slack/webhook_config.go
+++ alert/notify/channel/slack/webhook_config.go
@@ -18,6 +18,7 @@ var DefaultMessageTemplate = template.Must(template.New("message").Parse(message
 
 type WebhookConfig struct {
 	MessageTemplate *template.Template
+	ColorTemplate   *template.Template // optional attachment color
 }
 
 var DefaultWebhookConfig = WebhookConfig{
@@ -27,17 +28,43 @@ var DefaultWebhookConfig = WebhookConfig{
 func prepareWebhookConfig(cfg WebhookConfig) webhook.Config {
 	cfg.MessageTemplate = cmp.Or(cfg.MessageTemplate, DefaultWebhookConfig.MessageTemplate)
 	return webhook.Config{
-		BodyFormat: bodyFormatFunc(cfg.MessageTemplate),
+		BodyFormat: bodyFormatFunc(cfg.MessageTemplate, cfg.ColorTemplate),
 	}
 }
 
-func bodyFormatFunc(t *template.Template) func(msg channel.Message) (body []byte, mimeType string) {
+func bodyFormatFunc(msgTemplate, colorTemplate *template.Template) func(msg channel.Message) (body []byte, mimeType string) {
 	return func(msg channel.Message) (body []byte, mimeType string) {
 		var msgTxt strings.Builder
-		err := t.Execute(&msgTxt, msg.Data)
-		if err != nil {
+		if err := msgTemplate.Execute(&msgTxt, msg.Data); err != nil {
 			return nil, "application/json"
 		}
+
+		if colorTemplate != nil {
+			var colorTxt strings.Builder
+			if err := colorTemplate.Execute(&colorTxt, msg.Data); err == nil && colorTxt.Len() > 0 {
+				body, _ = json.Marshal(struct {
+					Attachments []struct {
+						Color    string   `json:"color"`
+						Text     string   `json:"text"`
+						MrkdwnIn []string `json:"mrkdwn_in"`
+					} `json:"attachments"`
+				}{
+					Attachments: []struct {
+						Color    string   `json:"color"`
+						Text     string   `json:"text"`
+						MrkdwnIn []string `json:"mrkdwn_in"`
+					}{
+						{
+							Color:    strings.TrimSpace(colorTxt.String()),
+							Text:     msgTxt.String(),
+							MrkdwnIn: []string{"text"},
+						},
+					},
+				})
+				return body, "application/json"
+			}
+		}
+
 		body, _ = json.Marshal(struct {
 			Text string `json:"text"`
 		}{
